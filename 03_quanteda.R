fa86df71153ce3bf261ba76ae5049aa1a54500e5
giampaolo montaletti <giampaolo@montaletti.com>
2025-02-02 12:10:21 +0100

    ampliato uso di quanteda

--- 03_quanteda.R
+++ 03_quanteda.R
@@ -1,29 +1,35 @@
-# install("quanteda/quanteda.corpora")
+# renv::install("quanteda/quanteda.dictionaries")
 library(renv)
+renv::status()
+renv::snapshot()
 # renv::init()
 library(quanteda)
 require(quanteda.textstats)
 require(quanteda.textplots)
 require(quanteda.corpora)
-
+# require(quanteda.dictionaries)
 Sys.setlocale(category = "LC_ALL", locale = "it_IT.UTF-8")
+quanteda_options(language_stemmer ="italian")
 
+apostrofo <- function (x) {
+  stringr::str_replace_all(x, "[\'\’](?!\\s)", "' ")
+}
+
+# genera corpus da lista ----
 
 mieitesti <- readRDS("data/mieitesti.rds")
-
 df <- data.frame(matrix(unlist(mieitesti), nrow=length(mieitesti), byrow=TRUE))
 names(df) <- c("data", "titolo", "testo")
-# setDT(df)
-
-
+df$codice <- make.names(paste(substring(df$titolo, 1, 11), df$data))
+df$testo <- paste(df$titolo, df$testo)
+df$testo <- apostrofo(df$testo)
 df$data <- as.Date(as.numeric(df$data))
-str(df)
 df <- df[df$data >= as.Date("2021-01-10"), ]
 
 mioc <- corpus(df
-               , docid_field = "titolo"
+               , docid_field = "codice"
                , text_field = "testo"
-               , list("data"))
+               , list("data", "titolo"))
 
 summary(mioc)
 
@@ -49,20 +55,33 @@ corp_sent_long <- corpus_subset(corp_sent, ntoken(corp_sent) >= 10)
 ndoc(corp_sent_long)
 
 # tokens()
-toks_immig <- tokens(mioc)
+# toks_immig <- tokens(mioc)
 toks <- tokens(mioc)
 
+#  tokens -----
 
-toks_nopunct <- tokens(mioc, remove_punct = TRUE, remove_numbers = TRUE)
-print(toks_nopunct)
+toks <- tokens(mioc, remove_punct = TRUE
+                       , remove_numbers = TRUE
+                       , remove_symbols = TRUE
+                       , remove_url = TRUE
+                       , remove_separators = TRUE
+                       , remove_hyphens = TRUE
+                       , split_hyphens = TRUE
+                       , padding = TRUE
+                       )
+print(toks)
+
+toks <- tokens_remove(toks, pattern = stopwords("it"), padding = TRUE)
+
 
 options(width = 110)
 
-kw_lavor <- kwic(toks_immig, pattern =  "lavor*")
+kw_lavor <- kwic(toks, pattern =  "lavor*")
 head(kw_lavor, 10)
 
-kw_immig2 <- kwic(toks_immig, pattern = c("lavor*", "disoc*"))
-head(kw_immig2, 10)
+kw_law2 <- kwic(toks
+                  , pattern = c("lavor*", "disoc*"))
+head(kw_law2, 10)
 
 
 toks_nostop <- tokens_select(toks, pattern = stopwords("it"), selection = "remove")
